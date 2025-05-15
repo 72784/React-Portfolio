e39fb784eb5c39b34297c312234fa515190862e6
Parth Shiroya <parthshiroya2001@gmail.com>
2025-05-15 13:10:07 -0700

    Margin change in resume download button

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,17 +13,17 @@ const Contact = () => {
         <motion.p         whileInView={{opacity:1,x:0}}
         initial={{opacity:0,x:60}}
         transition={{duration:1}}
-        className="my-4">{CONTACT.phoneNo}</motion.p>
+        className="my-2">{CONTACT.phoneNo}</motion.p>
         <div>
         <a
-          className="my-10 inline-block text-purple-300 underline hover:text-purple-400"
+          className="my-2 inline-block text-purple-300 underline hover:text-purple-400"
           href="/Resume_Parth.pdf"
           download
         >
         Download Resume
         </a>
         </div>
-        <a href="#" className="border-b my-4">{CONTACT.email}</a>
+        <a href="#" className="border-b my-2">{CONTACT.email}</a>
         
         
       </div>
